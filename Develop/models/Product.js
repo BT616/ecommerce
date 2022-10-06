ddb428cd7c90cd6466d94212760dc748fc0a982b
BT616 <110855674+BT616@users.noreply.github.com>
2022-10-06 16:15:43 -0700

    setup

---
+++ Develop/models/Product.js
@@ -0,0 +1,35 @@
+// import important parts of sequelize library
+const { Model, DataTypes, INTEGER } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    product_name: {
+      type: DataTypes.STRING},
+
+    price:{ 
+      type: INTEGER,},
+
+    stock:{
+      type: 
+      DataTypes.INTEGER},
+    
+    category_id:{ 
+      type: DataTypes.INTEGER}
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
