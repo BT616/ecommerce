092e72768bde12f7fe8b5bc73a43fd53af8a578e
BT616 <110855674+BT616@users.noreply.github.com>
2022-10-07 16:25:11 -0700

    filling in product

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const seedTags = require('../seeds/tag-seeds');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+foreignKey:'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product,{
+foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+through:{
+  model: ProductTag
+}
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag,{
+
+})
 
 module.exports = {
   Product,
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,46 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findAll(
+    include=
+    [{
+       model: Category,
+      attributes: ['id','category_name']
+       },
+       {
+        model: Tag,
+        atttributes: ['id','tag_name'],
+        through: {
+          model: ProductTag,
+          attributes:['id','product_id','tag'],
+          as:'product_tags'
+        }
+       }
+  ]);
+    res.status(200).json(productData);
+
+  }catch (err){
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+try{
+  const productData = await Product.findByPk(req.perams.id,{
+    include:[{ model: Category },{model: Tag}],
+  });
+  res.status(200).json(productData);
+} catch (err){
+  res.status(500).json(err);
+}
+
 });
 
 // create new product
