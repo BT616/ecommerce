4e2eea02971e8862693eec3fbcd04e97cdbe5143
BT616 <110855674+BT616@users.noreply.github.com>
2022-10-13 18:09:03 -0700

    corrected paths

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findOne(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
@@ -44,10 +44,11 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
-  console.log("line 50 category routes", req, res)
+  //console.log("line 50 category routes", req, res)
   const { category_name } = req.body
   try {
-    const categoryData = await Category.create(req.body.category_name);
+    
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
